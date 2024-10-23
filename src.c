babaed5c86e242447ffef8f7f214c9e748d48010
willbtty <115966859+willbtty@users.noreply.github.com>
2024-10-22 20:31:25 -0500

    Added support for cd

--- src.c
+++ src.c
@@ -38,7 +38,7 @@ char **test_get_input(char *input) {
         index++;
         parsed = strtok(NULL, separator);
     }
-    if (strncmp(parsed, "|", 1) == 0)
+    if (parsed != NULL && strncmp(parsed, "|", 1) == 0)
         isPipes = 1;
     else
         isPipes = 0;
@@ -49,32 +49,61 @@ char **test_get_input(char *input) {
 
 int main() {
     char **command;
-    char *input;
-    //char input[1024];
+    //char *input;
+    int size = 1024;
+    char input[size];
     //char strinput;
-    pid_t child_pid;
     int stat_loc;
-    int p[2];
-    pipe(p);
+
 
     while(1){
-        input = readline("[QUASH]$ ");
-        //printf("[QUASH]$ ");
-        //fgets(input, 1024, stdin);
-        command = test_get_input(input);
-        
-        // EX. command = ["ls", "|", "grep", "src"]
 
-        pid_t child_pid = fork();
-        if (child_pid == 0) {
-            execvp(command[0], command);
-            printf("Won't be printed if execvp is succesful\n");
-        } else {
-            waitpid(child_pid, &stat_loc, WUNTRACED);
+        //input = readline("[QUASH]$ ");
+        printf("[QUASH]$ ");
+        fgets(input, 1024, stdin);
+
+        // Remove trailing newline character if present
+        size_t len = strlen(input);
+        if (input[len - 1] == '\n') {
+            input[len - 1] = '\0';
         }
 
-        free(command);
-        free(input);
+        command = test_get_input(input);
+        // EX. command = ["ls", "|", "grep", "src"]
+
+        if (command[0] != NULL && strcmp(command[0], "cd") == 0)
+        {
+            if (command[1] == NULL) {
+                printf("cd: command not found\n");
+            }
+            else {
+                if (chdir(command[1]) != 0) {
+                    perror("cd");
+                }
+            }
+        }
+
+        else {
+            pid_t child_pid = fork();
+            if (child_pid == 0) {
+                execvp(command[0], command);
+                printf("Won't be printed if execvp is succesful\n");
+            } else {
+                waitpid(child_pid, &stat_loc, WUNTRACED);
+            }
+
+            if (isPipes == 1)
+            {
+                int p[2];
+                pipe(p);
+
+                for (int i = 1; i < sizeof(command); i++) // Shift elements to the left deleting the first element
+                    command[i-1] = command[i];
+            }
+
+            free(command);
+        }
+
     }
     return 0;
 }
